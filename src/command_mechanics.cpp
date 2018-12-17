84eecc9d121f796b7647195f632ec1845820fd2e
quantumpencil <quantumpencil@gmail.com>
2018-12-17 10:10:21 -0800

    mostly working

--- src/command_mechanics.cpp
+++ src/command_mechanics.cpp
@@ -4,35 +4,26 @@
 SISTERRAY_API void CommandMainRewrite(u8* cmd) {
     u8 character_id;
     u32 attacker_id;
-    u32 command_index;
+    u8 command_index;
 
     attacker_id = gDamageContextPtr->attackerID;
     character_id = gAiActorVariables[attacker_id].characterID;
-    command_index = gDamageContextPtr->commandIndex;
+    command_index = cmd[3];
 
+    
     if (attacker_id < 3) {
-        cmd[3] = CMD_DOUBLE_CUT;
-        if ((command_index == CMD_ATTACK) && (character_id == ACT_CLOUD)) {
-            /*When Cloud uses a physical attack, turn it into a double cut*/
-            if (TriggerCloudSpecial()) {
-                gDamageContextPtr->commandIndex = CMD_DOUBLE_CUT;
-            }
+        /*remove the command index check and see queue behavior working*/
+        if ((character_id == ACT_CLOUD) && (command_index == CMD_ATTACK)) {
+             cmd[3] = CMD_DOUBLE_CUT;
+             gDamageContextPtr->commandIndex = CMD_DOUBLE_CUT;
+
+             u8 targetMask = gDamageContextPtr->targetMaskCopy;
+             enqueueAction(attacker_id, (u16)0, CMD_DEATHBLOW, (u8)0, (u8)targetMask);
+           
         }
     }
-
     /*Call the games existinct Command Main Function*/
     oldCommandMain(cmd);
-
-    if (0 && attacker_id < 3) {
-        if ((command_index == CMD_ATTACK) && (character_id == ACT_CLOUD)) {
-            /*Enqueue a potential extra action with priority 0 for execution*/
-            if (TriggerCloudSpecial()) {
-                /*Populate with the correct arguments*/
-                u8 targetMask = gDamageContextPtr->targetMaskCopy;
-                enqueueAction(attacker_id, (u16)0, CMD_DEATHBLOW, (u8)0, (u8)targetMask);
-            }
-        }
-    }
 }
 
 /*Determine whethe or not Cloud's special actions trigger*/

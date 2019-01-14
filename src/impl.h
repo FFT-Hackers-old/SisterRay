c570cbdb7524a88ffe5e73c47cfa6ef8681a86e5
quantumpencil <quantumpencil@gmail.com>
2019-01-13 20:22:50 -0800

    tactical-elements stripped out

--- src/impl.h
+++ src/impl.h
@@ -14,21 +14,6 @@ typedef struct {
 
 SISTERRAY_GLOBAL SrContext gContext;
 
-typedef void (PFNCOMMANDMAINPROC)(u8*);
-SISTERRAY_GLOBAL PFNCOMMANDMAINPROC* oldCommandMain;
-
-typedef void (PFNRUNANIMSCRIPT)(u16, u32, u32, u32);
-SISTERRAY_GLOBAL PFNRUNANIMSCRIPT* oldRunAnimationScript;
-
-typedef void (PFNDECREMENTCOUNTERS)();
-SISTERRAY_GLOBAL PFNDECREMENTCOUNTERS* oldDecrementCounters;
-
-typedef void (PFNAPPLYDAMAGE)();
-SISTERRAY_GLOBAL PFNAPPLYDAMAGE* oldApplyDamage;
-
-//typedef void (PFNANIMEFFECT)(u8);
-//SISTERRAY_GLOBAL PFNANIMEFFECT* oldAnimEffectLookUp;
-
 SISTERRAY_API void InitMateria(SrKernelStream* stream);
 
 SISTERRAY_API void InitLog(void);

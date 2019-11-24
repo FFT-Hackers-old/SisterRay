a5e584c9eeb4502eae79d7185996870b3fb80cdd
quantumpencil <quantumpencil@gmail.com>
2019-11-23 23:07:50 -0800

    initial commit

--- src/battle/engine/animation_script_parser.h
+++ src/battle/engine/animation_script_parser.h
@@ -4,3 +4,22 @@
 #include <SisterRay/SisterRay.h>
 
 #endif // !ANIMATION_SCRIPT_PARSER_H
+
+#pragma pack(push, 1)
+typedef struct {
+    u8 isScriptActive;
+    u8 currentOpCode;
+    u8 field_2;
+    u8 field_3;
+    u8* scriptPtr;
+    u8 field_8;
+    u8 field_9;
+    u16 opCodeArgs[5];
+    u8 field_14;
+    u8 field_15;
+    u16 field_16;
+    u16 field_18;
+    u16 field_1A;
+    u8 field_1C
+}GameAnimationScriptContext
+#pragma pack(pop)
